36d0009959fccdccc139080652016ea95319d29b
Nicholas Galante <nicholasgalante94@gmail.com>
2023-01-16 14:35:22 -0500

    Create fetch request

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+   const [image, setImage] = useState("");
+   const [isLoaded, setIsLoaded] = useState(false);
+
+   useEffect(() => {
+      fetch("https://dog.ceo/api/breeds/image/random")
+         .then(res => res.json())
+         .then(data => {
+            setImage(data.message);
+            setIsLoaded(true);
+         })
+   }, [])
+
+   if(!isLoaded) return <h3>Loading...</h3>
+
+   return (
+      <div>
+         <img src={image} />
+      </div>
+   )
+}
+
+export default App;
